27a6df5a9773ba6cf43c25c9671cc0138a3ce544
rum <rumeshranaweera99@gmail.com>
2023-06-15 23:24:47 +0530

    add checkoutProduct component

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CheckoutProduct from "@/components/checkoutProduct";
 import PageDiv from "@/components/pageDiv";
 import SectionTitle from "@/components/sectionTitle";
 import { useStore } from "@/store/store";
@@ -70,25 +71,3 @@ const Checkout = () => {
 };
 
 export default Checkout;
-
-export const CheckoutProduct = ({
-  title,
-  price,
-  place,
-}: {
-  title: string;
-  price: number;
-  place: string;
-}) => {
-  return (
-    <div className="flex justify-between p-2 border border-yellow-400">
-      <div className="text-left">
-        <h4 className="text-xl font-bold">{title}</h4>
-        <h6>location :{place}</h6>
-      </div>
-      <div>
-        <h4 className="text-xl font-bold">${price}</h4>
-      </div>
-    </div>
-  );
-};
