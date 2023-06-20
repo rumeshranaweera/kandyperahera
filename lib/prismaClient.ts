e1a2388d32919b6bb988507091204611555a7e39
rum <rumeshranaweera99@gmail.com>
2023-06-20 20:30:22 +0530

    comment prismaClient functions

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -3,22 +3,19 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// export async function addNewExplore(data: FormValue) {
-//   // ... you will write your Prisma Client queries here
-//   return prisma.exploreItem.create({ data });
+export async function addNewExplore(data: FormValue) {
+  return prisma.exploreItem.create({ data });
+}
+// export async function addMultipleExplore(data: FormValue[]) {
+//   return prisma.exploreItem.createMany({ data });
 // }
-export async function addMultipleExplore(data: FormValue[]) {
-  // ... you will write your Prisma Client queries here
-  return prisma.exploreItem.createMany({ data });
-}
-export async function getExploreData() {
-  // ... you will write your Prisma Client queries here
-  return prisma.exploreItem.findMany();
-}
-
-export async function deleteAllExploreData() {
-  return prisma.exploreItem.deleteMany();
-}
+// export async function getExploreData() {
+//   return prisma.exploreItem.findMany();
+// }
+//
+// export async function deleteAllExploreData() {
+//   return prisma.exploreItem.deleteMany();
+// }
 
 // client()
 //   .then(async (data) => {
